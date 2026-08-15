6073c21b18978f903d836e156a7d0ad76f2f81c1
Aethra-Comp <princemaurya8879@gmail.com>
2026-08-15 13:17:27 +0530

    curd done in array list

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,13 +2,15 @@ package com.example.demo.controller;
 import java.util.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.User;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/users")
@@ -36,6 +38,42 @@ public class UserController {
     public User create(@RequestBody User user) {
         users.add(user);
         return user;
+    }
+    
+    @PutMapping("/update/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User obj_update) {
+        // for(User user : users) {
+        //     if(user.getId() == id) {
+        //         user.setName(obj_update.getName());
+        //         user.setAge(obj_update.getAge());
+        //     }
+        // }
+        // return getUser(id);
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .map(user -> {
+                    user.setName(obj_update.getName());
+                    user.setAge(obj_update.getAge());
+                    return user;
+                })
+                .orElse(null);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        boolean removed = users.removeIf(user -> user.getId() == id);
+
+        if(removed) {
+            return "User with ID " + id + " deleted successfully.";
+        } else {
+            return "User with ID " + id + " not found.";
+        }
+    }
+
+    @GetMapping()
+    public List<User> getAllUsersPrince() {
+        return users;
     }
     
 }
